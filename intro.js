589af1b7b57ee605ff14c40cf8f905eba318750a
Stephen <stephenduahopoku@gmail.com>
2026-09-11 01:46:05 +0000

    Complete assignment details

--- intro.js
+++ intro.js
@@ -1,3 +1,3 @@
 console.log('My name is Stephen');
 console.log('I\'m 21yrs old ');
-console.log('My favorite programming language is python');
+console.log('My favorite programming language is python_'); 
