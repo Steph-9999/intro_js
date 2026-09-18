eb256cd7f4bc373b1bb04bd818821239771c278c
Stephen <stephenduahopoku@gmail.com>
2026-09-18 15:03:27 +0000

    Rename assignment_2 folder to week2-node-express

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(new Date(), req.method, req.url);
+  next();
+});
+
+
+app.get('/user/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  res.send(id);
+});
+
+app.get('/search', (req, res) => {
+  const id = req.query.id;
+  console.log(id);
+  res.send(id);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
